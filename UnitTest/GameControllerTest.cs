55b4c8148351984bd9c25553cc103211227f9eb7
Marijn <marijnvandenbos59@gmail.com>
2023-06-13 16:57:38 +0200

    added more testcases

---
+++ UnitTest/GameControllerTest.cs
@@ -0,0 +1,51 @@
+using Sudoku.controler.GameController;
+using Sudoku.controler.InputController;
+using Sudoku.data.Boards;
+using Sudoku.data.Boards.@abstract;
+using Sudoku.data.Boards.Enum;
+using Sudoku.data.EditorStates;
+using Sudoku.data.Game;
+using Sudoku.data.Game.Enum;
+using Sudoku.view.GameView;
+using Sudoku.view.Sudoku_board;
+
+namespace Sudoku.Tests;
+
+public class GameControllerTest
+{
+    GameController gameController;
+    GameContext game;
+    private Board board;
+    public GameControllerTest()
+    {
+        board = new NineByNine("700509001000000000150070063003904100000050000002106400390040076000000000600201004",
+            SudokuDisplayMode.Assist);
+        game = new GameContext(new GameSolvingState(),board, DisplayOptions.Easy, EditorState.Defenitive);
+        new GameController(new InputHandlerController(game), new SudokuGameView(game, new NineByNineBoardDrawingStrategy(board)), game);
+    }
+    
+    
+    [Fact]
+    public void gameController_solveGame_gameIsSolved()
+    {
+        // Arrange
+        gameController = new GameController(new InputHandlerController(game), new SudokuGameView(game, new NineByNineBoardDrawingStrategy(board)), game);
+        // Act
+        gameController.Game.solve();
+        // Assert
+        Assert.Equal(GameStatus.Finished, game.GameStatus);
+    }
+    
+    [Fact]
+    public void gameController_solveGame_gameIsNotSolved()
+    {
+        // Arrange
+        gameController = new GameController(new InputHandlerController(game), new SudokuGameView(game, new NineByNineBoardDrawingStrategy(board)), game);
+        Console.Title = "GameControllerTest";
+        // Act
+        Assert.thgameController.Game.solve();
+        // Assert
+        Assert.NotEqual(GameStatus.Ongoing, game.GameStatus);
+    }
+    
+}
---
+++ UnitTest/InputControllerTest.cs
@@ -0,0 +1,45 @@
+using Sudoku.controler.InputController;
+using Sudoku.data.Boards;
+using Sudoku.data.Boards.@abstract;
+using Sudoku.data.Boards.Enum;
+using Sudoku.data.EditorStates;
+using Sudoku.data.Game;
+using Sudoku.data.Game.Enum;
+using Sudoku.data.Input.Enum;
+
+namespace Sudoku.Tests;
+
+public class InputControllerTest
+{
+    InputHandlerController inputHandlerController;
+    private GameContext game;
+    public InputControllerTest()
+    {
+        inputHandlerController = new InputHandlerController(new GameContext(new GameSolvingState(), new NineByNine("080030709190800000000700002016080000200905003000010920800007000000008095309050060", SudokuDisplayMode.Simple), DisplayOptions.Easy, new EditorState()));
+    }
+
+    [Fact]
+    public void inputController_inValidInput_doensNotchangeGameDisplay()
+    {
+       // Arrange
+       inputHandlerController.Subscribe(inputHandlerController.Game);
+       game = inputHandlerController.Game;
+       // Act
+       inputHandlerController.SetPlayerInput("test");
+       // Assert
+       Assert.Equal(inputHandlerController.Game.DisplayOption, game.DisplayOption);
+       
+    }
+    
+    [Fact]
+    public void inputController_validInput_changeGameDisplay()
+    {
+        // Arrange
+        inputHandlerController.Subscribe(inputHandlerController.Game);
+        game = inputHandlerController.Game;
+        // Act
+        inputHandlerController.SetPlayerInput("t");
+        // Assert
+        Assert.NotSame(inputHandlerController.Game.DisplayOption, game.DisplayOption);
+    }
+}
