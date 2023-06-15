c5e12ae9aa45966cd57ba880d46dd77aa6c12ff0
Marijn <marijnvandenbos59@gmail.com>
2023-06-15 12:01:01 +0200

    updated puzzel formating

--- Sudoku.view/Cell/CellView.cs
+++ Sudoku.view/Cell/CellView.cs
@@ -47,7 +47,7 @@ public class CellView : IView
         
         else
         {
-            // _helperNumberLeaf.Draw();
+            
             for (int i = 0; i < 9; i++)
             {
                 Console.Write(" ");
