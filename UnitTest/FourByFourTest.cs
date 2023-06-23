195f3efb310e11d6f7857baf8e6865b4ea6c77ae
Marijn <marijnvandenbos59@gmail.com>
2023-06-23 15:11:05 +0200

    added sixbysix and four by four

--- UnitTest/FourByFourTest.cs
+++ UnitTest/FourByFourTest.cs
@@ -1,5 +1,70 @@
-namespace UnitTest;
+using Sudoku.data.Boards;
+using Sudoku.data.Boards.Enum;
+using Xunit;
 
-public class FourByFourTests
+namespace UnitTest
 {
+    public class FourByFourTest
+    {
+        private string Cells { get; set; }
+        private FourByFour _fourByFour;
+        private SudokuDisplayMode _sudokuDisplayMode = SudokuDisplayMode.Assist;
+
+        public FourByFourTest()
+        {
+            Cells = "0340400210030210";
+        }
+
+        [Fact]
+        public void CreateBoard_ValidInput_ReturnsCorrectBoard()
+        {
+            // Arrange
+            _fourByFour = new FourByFour(Cells, _sudokuDisplayMode);
+
+            // Act
+            var board = _fourByFour.CreateBoard(Cells);
+
+            // Assert
+            Assert.Equal(4, board.Count);
+            Assert.Equal(4, board[0].Count);
+        }
+
+        [Fact]
+        public void CreateBoard_InvalidInput_ThrowsException()
+        {
+            // Arrange
+            _sudokuDisplayMode = SudokuDisplayMode.Assist;
+            Cells = "";
+
+            // Act & Assert
+            Assert.Throws<IndexOutOfRangeException>(() => new FourByFour(Cells, _sudokuDisplayMode));
+        }
+
+        [Fact]
+        public void Copy_Validinput_ReturnsCopyOfBoard()
+        {
+            // Arrange
+            _fourByFour = new FourByFour(Cells, _sudokuDisplayMode);
+
+            // Act
+            var copy = _fourByFour.copy();
+
+            // Assert
+            Assert.Equivalent(_fourByFour.Cells, copy.Cells); 
+        }
+
+        [Fact]
+        public void Init_InvokesSolver_ReturnsSolvedBoard()
+        {
+            // Arrange
+            _fourByFour = new FourByFour(Cells, _sudokuDisplayMode);
+
+            // Act
+            _fourByFour.init();
+
+            // Assert
+            Assert.NotEqual(_fourByFour.Cells, _fourByFour.SolvedBoard.Cells);
+        }
+
+    }
 }
--- UnitTest/SixBySixTest.cs
+++ UnitTest/SixBySixTest.cs
@@ -1,5 +1,70 @@
-namespace UnitTest;
+using Sudoku.data.Boards;
+using Sudoku.data.Boards.Enum;
+using Xunit;
 
-public class SixBySixTest
+namespace UnitTest
 {
+    public class SixBySixTest
+    {
+        private string Cells { get; set; }
+        private SixBySix _sixBySix;
+        private SudokuDisplayMode _sudokuDisplayMode = SudokuDisplayMode.Assist;
+
+        public SixBySixTest()
+        {
+            Cells = "640200051000364002500436000640006025";
+        }
+
+        [Fact]
+        public void CreateBoard_ValidInput_ReturnsCorrectBoard()
+        {
+            // Arrange
+            _sixBySix = new SixBySix(Cells, _sudokuDisplayMode);
+
+            // Act
+            var board = _sixBySix.CreateBoard(Cells);
+
+            // Assert
+            Assert.Equal(6, board.Count);
+            Assert.Equal(6, board[0].Count);
+        }
+
+        [Fact]
+        public void CreateBoard_InvalidInput_ThrowsException()
+        {
+            // Arrange
+            _sudokuDisplayMode = SudokuDisplayMode.Assist;
+            Cells = "";
+
+            // Act & Assert
+            Assert.Throws<IndexOutOfRangeException>(() => new SixBySix(Cells, _sudokuDisplayMode));
+        }
+
+        [Fact]
+        public void Copy_Validinput_ReturnsCopyOfBoard()
+        {
+            // Arrange
+            _sixBySix = new SixBySix(Cells, _sudokuDisplayMode);
+
+            // Act
+            var copy = _sixBySix.copy();
+
+            // Assert
+            Assert.Equivalent(copy.Cells, _sixBySix.Cells);
+        }
+
+        [Fact]
+        public void Init_InvokesSolver_ReturnsSolvedBoard()
+        {
+            // Arrange
+            _sixBySix = new SixBySix(Cells, _sudokuDisplayMode);
+
+            // Act
+            _sixBySix.init();
+
+            // Assert
+            Assert.NotEqual(_sixBySix.Cells, _sixBySix.SolvedBoard.Cells);
+        }
+
+    }
 }
